9010cd26b6bbe64c03b54350b5aef2b0b093d87f
ex0ns <g.tournigand@gmail.com>
2017-11-03 13:29:35 +0100

    Remove unused token parsing

--- parser.js
+++ parser.js
@@ -37,23 +37,12 @@ class Parser {
     }));
   }
 
-  static async _extractTokens(page) {
-    const viewToken = await page.evaluate(id => 
-      document.getElementById("j_id1:javax.faces.ViewState:0").value);
-  
-    const cookies = await page.cookies();
-    const sessionID = cookies.filter((cookie) => cookie.name === "JSESSIONID").map((cookie) => cookie.value)[0];
-    
-    return  {sessionID, viewToken};
-  }
-
   static async parse() {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://snow.myswitzerland.com/bulletin_enneigement/');
     await page.click("#check02");
    
-    const {sessionID, viewToken} = await Parser._extractTokens(page);
     await Parser._loadAll(page);
     const stations = await Parser._extractStationData(page);
   
